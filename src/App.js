b9554b91364a34e18d156ce30c882a26741eecf8
Danlois Tovar <danlois.tovar@gmail.com>
2022-08-26 21:15:36 -0300

    limpiando proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
             <h1>Hola Mundo!!!</h1>
+            <p>Esto de un texto!!</p>
         </div>
     );
 }
